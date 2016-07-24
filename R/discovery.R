f382cd2db678a29a4232bf01e5d8eb41af2ce415
Mark Edmondson <github@markedmondson.me>
2016-07-24 03:03:39 +0200

    creation of working auto short URL
    the methods in the discovery URL move though, need reliable way to
    locate methods.

--- R/discovery.R
+++ R/discovery.R
@@ -83,13 +83,20 @@ add_line <- function(line, path, quiet = FALSE) {
 #' 
 #' @param filename R file to write skeleton to
 #' @param api_json The json from \link{gar_discovery_api}
+#' @param api_method Location of API resource methods
 #' 
 #' @return TRUE if successful, side effect will write a file
 #' @export
 gar_create_api_skeleton <- function(filename = "./R/new_api.R", 
-                                    api_json = gar_discovery_api("urlshortener","v1")){
+                                    api_json = gar_discovery_api("urlshortener","v1"),
+                                    api_method = api_json$resources$url$methods){
   
-  stopifnot(api_json$kind == "discovery#restDescription")
+
+  if(is.null(api_json$kind) && api_json$kind != "discovery#restDescription"){
+    stop("api_json not recognised from gar_discovery_api")
+  }
+  
+  temp <- tempfile()
   
   if(file.exists(filename)){
     stop("File: ", filename, " exists. Delete it and run again")
@@ -102,22 +109,52 @@ gar_create_api_skeleton <- function(filename = "./R/new_api.R",
       "#' @docType package \n",
       "#' @name ", paste0(api_json$name,"_googleAuthR"), "\n",
       "#' \n",
-      "NULL"
+      "NULL",
+      "\t\n"
       )
   
-  add_line(header, filename)
+  add_line(header, temp)
   
-  fb <- lapply(api_json$resources$url$methods, function_body, api_json = api_json)
+  ## apis can have methods at varying steps into JSON tree
+  ## find the methods to extract into API
+  
+  fd <- lapply(api_method, function_docs, api_json = api_json)
+  fp <- lapply(api_method, function_params, api_json = api_json)
+  fb <- lapply(api_method, function_body, api_json = api_json)
 
-  lapply(fb, add_line, filename)
+  lapply(paste(fd, fp,fb, sep = "\t\n"), add_line, temp)
+  
+  formatR::tidy_eval(temp, blank = TRUE, file = filename, width.cutoff = 80)
+  file.remove(temp)
 }
 
 function_docs <- function(api_json_resource_method, api_json){
   
+  docs <- paste0(
+    "\n",
+    "#' ", api_json_resource_method$description, "\n",
+    "#' \n",
+    "#' ", paste(collapse = "\n#' ", sep = "\n#'",
+                 lapply(names(api_json_resource_method$parameters), 
+                        make_vars_description, 
+                        api_json_resource_method=api_json_resource_method$parameters)
+                 ),
+    "#' @importFrom googleAuthR gar_api_generator\n",
+    "#' @export\n"
+  )
+  
+  docs
 }
 
 function_params <- function(api_json_resource_method, api_json){
   
+  func <- paste0(
+    gsub(paste0(api_json$name,"."), "", api_json_resource_method$id), " <- function(",
+    paste(make.names(names(api_json_resource_method$parameters)), sep = "\n", collapse = ",\n"),
+    "){\n\n"
+  )
+  
+  func
 }
 
 function_body <- function(api_json_resource_method, api_json){
@@ -128,11 +165,13 @@ function_body <- function(api_json_resource_method, api_json){
   fb <- paste0(
     "url <- '", paste0(api_json$baseUrl, api_json_resource_method$path), "'\n",
     api_json_resource_method$id, " <- gar_api_generator(url, \n",
-    "\t\t\t\t'",api_json_resource_method$httpMethod,"'\n",
+    "\t\t\t\t'",api_json_resource_method$httpMethod,"',\n",
     "\t\t\t\t", path_list,
     "\t\t\t\t", pars_list,
     "\t\t\t\t", "data_parse_function = function(x) x)\n\n",
-    api_json_resource_method$id,"()"
+    api_json_resource_method$id,"()",
+    "\n",
+    "}\t\n"
   )
   
   fb
@@ -149,17 +188,23 @@ make_pathpars_list <- function(api_json_resource_method,
   out <- switch(type,
       path = paste0(
         "path_args = list(\n",
-        paste(collapse = ",\n", 
-              lapply(names(api_json_resource_method$parameters), make_path_pars_entry, "path", api_json_resource_method=api_json_resource_method)
+        paste(collapse = ",", 
+              lapply(names(api_json_resource_method$parameters), 
+                     make_path_pars_entry, 
+                     "path", 
+                     api_json_resource_method=api_json_resource_method)
               ),
-        ")\n"
+        "),\n"
       ),
       query = paste0(
         "pars_args = list(\n",
-        paste(collapse = ",\n", 
-              lapply(names(api_json_resource_method$parameters), make_path_pars_entry, "query", api_json_resource_method=api_json_resource_method)
+        paste(collapse = ",", 
+              lapply(names(api_json_resource_method$parameters), 
+                     make_path_pars_entry, 
+                     "query", 
+                     api_json_resource_method=api_json_resource_method)
               ),
-        ")\n" 
+        "),\n" 
       )
       )
   
@@ -174,11 +219,19 @@ make_path_pars_entry <- function(x,
   type <- match.arg(type)
 
   if(api_json_resource_method$parameters[[x]]$location == type){
-    out <- paste0("\t\t",x, " = ", x, "\n", collapse = "")
+    out <- paste0("\t\t`",x, "` = ", make.names(x), "\n", collapse = "")
   } else {
     out <- NULL
   }
   
   out
+  
+}
+
+make_vars_description <- function(x, 
+                                 api_json_resource_method){
+  
+  paste0("@param ", make.names(x), " ", api_json_resource_method[[x]]$description,"\n", 
+         collapse = "")
   
 }
