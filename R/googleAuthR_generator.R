bbb19cc7f286d9ce9bd19bc2a7bd01e4da86b691
Mark Edmondson <github@markedmondson.me>
2016-08-28 13:00:58 +0200

    revert parsed_content
    This broke stuff that relied on it

--- R/googleAuthR_generator.R
+++ R/googleAuthR_generator.R
@@ -144,7 +144,7 @@ gar_api_generator <- function(baseURI,
                              simplifyVector=simplifyVector)
 
         if(!is.null(data_parse_function)){
-          reqtry <- try(data_parse_function(req$parsed_content, ...))
+          reqtry <- try(data_parse_function(req$content, ...))
           if(any(is.error(reqtry), is.null(reqtry))){
             warning("API Data failed to parse.  Returning parsed from JSON content.
                     Use this to test against your data_parse_function.")
@@ -335,7 +335,7 @@ doHttrRequest <- function(url,
       content <- httr::content(req, as = "text", type = "application/json",encoding = "UTF-8")
       content <- jsonlite::fromJSON(content,
                                     simplifyVector = simplifyVector)
-      req$parsed_content <- content
+      req$content <- content
     }
   } else {
     myMessage("No checks on content due to option googleAuthR.rawResponse, returning raw", level=2)
